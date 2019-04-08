29d6fa01302433a4596daf6e46f4762cccd459bf
Jules Kirk <kirkjules.jk@gmail.com>
2019-04-08 17:14:54 +0700

    editing candles.py

--- api/scripts/candles.py
+++ api/scripts/candles.py
@@ -19,9 +19,10 @@ from api import oanda
               help="The number of candlesticks to return in the reponse.\
               Count should not be specified if both the start and end\
               parameters are provided, as the time range combined with the\
-              graularity will determine the number of candlesticks to\
+              granularity will determine the number of candlesticks to\
               return.")
-@click.option("--from",
+@click.option("-f", "--from", "from_",
+              default="1993-08-18T09:32:13Z",
               type=click.DateTime(formats="%Y-%m-%dT%H:%M:%SZ"),
               help="The start of the time range to fetch candlesticks for.")
 @click.option("--to",
@@ -47,16 +48,30 @@ from api import oanda
               default="Friday",
               type=click.STRING,
               help="")
-def clickData(ticker, price, granularity, count, from, to, smooth,
+@click.argument("--ticker")
+@click.argument("cF", type=click.File("rb"))
+def clickData(ticker, price, granularity, count, from_, to, smooth,
               includeFirst, dailyAlignment, alignmentTimezone,
-              weeklyAlignment):
-
-    ticker = "EUR_USD"
-    arguments = {"count": "6", "price": "M", "granularity": "S5"}
-    r = oanda.Instrument("api/config.ini")
+              weeklyAlignment, cF):
+    arguments = {"price": price,
+                 "granularity": granularity,
+                 "count": count,
+                 "from": from_,
+                 "to": to,
+                 "smooth": smooth,
+                 "includeFirst": includeFirst,
+                 "dailyAlignment": dailyAlignment,
+                 "alignmentTimezone": alignmentTimezone,
+                 "weeklyAlignment": weeklyAlignment}
+    if arguments["from"] == "1993-08-18T09:32:13Z":
+        del arguments["from"]
+        del arguments["to"]
+    else:
+        del arguments["count"]
+    r = oanda.Instrument(cF)
     data = r.candles(ticker, arguments)
     click.echo(data.json())
 
 
-if __name__ == '__main__':
-    clickData()
+# if __name__ == '__main__':
+#    clickData()
