4eb61cb8a921d6b323322f38eb536fb1020c3d41
Jules Kirk <kirkjules.jk@gmail.com>
2019-04-10 16:15:43 +0700

    adding output functionality to oanda.py

--- api/oanda.py
+++ api/oanda.py
@@ -1,5 +1,6 @@
 import configparser
 import requests
+import pandas as pd
 
 
 class Api:
@@ -15,21 +16,54 @@ class Api:
 
 class Instrument(Api):
 
-    def candles(self, instrument, queryParameters):
+    def candles(self, instrument, queryParameters, outType=None,
+                outDir=None):
         """Request query parameters are: price, granularity, count,
         from, to, smooth, includeFirst, dailyAlignment, alignmentTimezone,
         weeklyAlignment.
         Refer to http://developer.oanda.com/rest-live-v20/instrument-ep/
         for parameter descriptions.
+
+        Optional arguments outType and outDir can be specified if the return
+        should not be a request object.
         """
 
         headers = {"Content-Type": "application/json",
                    "Authorization": "Bearer {0}".format(self.key)}
+
         url = "https://api-fxpractice.oanda.com/v3/instruments/{0}/candles?"
 
-        return requests.get(url.format(instrument),
-                            headers=headers,
-                            params=queryParameters)
+        r = requests.get(url.format(instrument),
+                         headers=headers,
+                         params=queryParameters)
+
+        if outType is None:
+            return r
+
+        elif outType == "json":
+            return r.json()
+
+        elif outType == "pandas" or outType == "csv":
+
+            dic = {}
+
+            cols = {"o": "open", "h": "high", "l": "low", "c": "close"}
+
+            for i in range(len(r.json()["candles"])):
+                dic[r.json()["candles"][i]["time"]] =\
+                        r.json()["candles"][i]["mid"]
+
+            df = pd.DataFrame.from_dict(dic,
+                                        orient="index").rename(columns=cols)
+
+            if outType == "pandas":
+                return df
+
+            elif outType == "csv":
+                return df.to_csv(outDir)
+
+        else:
+            print("Error")
 
 
 if __name__ == "__main__":
