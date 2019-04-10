9dd1d4903743c8257ffe726857c8ab168c45f9a8
Jules Kirk <kirkjules.jk@gmail.com>
2019-04-10 08:45:27 +0700

    creating first test for oanda.candles()

---
+++ tests/testapi/testoanda/testcandles.py
@@ -0,0 +1,19 @@
+import unittest
+from api import oanda
+
+
+class TestCandles(unittest.TestCase):
+    def test_response(self):
+        """
+        Test HTTP response type.
+        """
+        ticker = "EUR_USD"
+        arguments = {"count": "6", "price": "M", "granularity": "S5"}
+        cf = "config.ini"
+        r = oanda.Instrument(cf)
+        data = r.candles(ticker, arguments)
+        self.assertEqual(data.status_code, 200)
+
+
+if __name__ == "__main__":
+    unittest.main()
