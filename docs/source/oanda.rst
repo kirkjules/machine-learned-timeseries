97ac171fa16d16a3cb9b10b3304540983a71a006
Jules Kirk <kirkjules.jk@gmail.com>
2019-06-06 15:34:50 +1000

    reset sphinx documentation source, api.oanda.Candles init docstring completed, class methods redefined as @classmethod for better implementation by user

---
+++ docs/source/oanda.rst
@@ -0,0 +1,3 @@
+.. automodule:: htp.api.oanda 
+    :members:
+
--- htp/api/oanda.py
+++ htp/api/oanda.py
@@ -1,32 +1,97 @@
+"""
+The oanda module contains functions that interact with the Oanda V20 API
+endpoints, docummented at http://developer.oanda.com/rest-live-v20/introduction
+"""
 import logging
 import requests
 import pandas as pd
-from . import Api, exceptions
+from htp.api import Api, exceptions
 
 log = logging.getLogger(__name__)
 
 
 class Candles(Api):
+    """
+    A request operation that query's the Oanda instrument.Candles endpoint for
+    ticker timeseries data.
+
+    The class inherits from Api, reading in API variables from a `.yaml`
+    configuration file which are required to complete a GET HTTP request to the
+    endpoint. A response object is initialised and, if the request is
+    successful will contain the target timeseries data. Data manipulation is
+    applied, as defined by class methods, to this object to yield differently
+    structured data sets.
+
+    Parameters
+    ----------
+    kwargs : str
+        Any extra keyword arguments that is required for class functionality.
+
+    Attributes
+    ---------
+    instrument :  str
+        A class attribute defined by a kwarg key, value pair with the key set
+        to `instrument` and the value a string denoting the ticker available
+        to being queried via the target endpoint.
+    url : str
+        The URL query string the defines the target endpoint.
+    queryParameters : dic
+        A class attribute defined by a kwarg, key, value pair with the key set
+        to `queryParameters` and the value a dictionary with keys matching the
+        available endpoint parameters and corresponding permitted value. See
+        Notes.
+    r : requests.models.Response
+        The response object that contains the intended ticker timeseries data,
+        as well as general HTTP response information such as the HTTP status
+        code.
+    status : int
+        A class attribute that exposes the HTTP status code in a separate
+        variable.
+
+    Raises
+    ------
+    requests.exceptions.RequestException
+        If the GET request has failed. This is a general exception defined in
+        the requests library and will catch any error being raised by that
+        library and any library that it uses. An example case is where
+        it was not possible to connect to the URL's server due to a lack of
+        network connection.
+
+    OandaException
+        If the response status code is not 200. Here, the endpoint has provided
+        a reason in the in the response body and this exposed by the exception.
+        An example case is where the authorization token is incorrect.
+
+    Notes
+    -----
+    The instrument.Candles endpoing posesses the following query parameters:
+    price, granularity, count, from, to, smooth, includeFirst, dailyAlignment,
+    alignmentTimezone, weeklyAlignment.
+    Refer to http://developer.oanda.com/rest-live-v20/instrument-ep/
+    for parameter descriptions.
+    Api behaviour will depend on Oanda releases. Notable mentions are:
+
+    * Parameters `from` and `to` set outside the datarange for what ticker
+      timeseries data is available will be handled by the endpoint by querying
+      the closest possible datetime values. The exception is where the `to`
+      paramater is in the future.
+    * Where an inappropriate granularity is used which exceeds the
+      maximum candle count value an error will be reported by the api
+      with a HTTP 400 status and logged by the Candles class.
+
+    See Also
+    --------
+    htp.api : Api
+
+    """
 
     def __init__(self, configFile="config.yaml", api="oanda",
                  access="practise", **kwargs):
-        """Request query parameters are: price, granularity, count,
-        from, to, smooth, includeFirst, dailyAlignment, alignmentTimezone,
-        weeklyAlignment.
-        Refer to http://developer.oanda.com/rest-live-v20/instrument-ep/
-        for parameter descriptions.
-        Api behaviour will depend on Oanda releases. Notable mentions are:
-            From and to parameters outside available ticker will return
-            closest datetime results.
-            Except where the to paramater is in the future.
-            Where an inappropriate granularity is used which exceeds the
-            maximum candle count value an error will be reported by the api
-            with a HTTP 400 status and logged by the Candles class.
-        """
-        super().__init__(configFile, api, access, **kwargs)
+        super().__init__(configFile, api, access)  # , **kwargs)
         self.headers = {"Content-Type": "application/json",
                         "Authorization": "Bearer {0}"
                         .format(self.details["token"])}
+        print(kwargs)
         self.instrument = kwargs["instrument"]
         self.url = self.details["url"] + "instruments/{0}/candles?".format(
             self.instrument)
@@ -52,18 +117,20 @@ class Candles(Api):
                   self.r.json(),
                   status_code=self.r.status_code)
 
-    def json(self):
-        return self.r.json()
+    @classmethod
+    def to_json(cls, **kwargs):
+        return cls(**kwargs).r.json()
 
-    def df(self, filename=None):
+    @classmethod
+    def to_df(cls, filename=None, **kwargs):
 
         dic = {}
 
         cols = {"o": "open", "h": "high", "l": "low", "c": "close"}
 
-        for i in range(len(self.r.json()["candles"])):
-            dic[self.r.json()["candles"][i]["time"]] =\
-                    self.r.json()["candles"][i]["mid"]
+        resp = cls.to_json(**kwargs)
+        for i in range(len(resp["candles"])):
+            dic[resp["candles"][i]["time"]] = resp["candles"][i]["mid"]
 
         data = pd.DataFrame.from_dict(dic, orient="index").rename(columns=cols)
 
@@ -82,4 +149,8 @@ if __name__ == "__main__":
     arguments = {"count": "6", "price": "M", "granularity": "S5"}
     cf = os.path.join(os.path.dirname(__file__), "../..", "config.yaml")
     data = Candles(configFile=cf, instrument=ticker, queryParameters=arguments)
-    pprint(data.json())
+    pprint(data.r.json())
+    pprint(Candles.to_json(configFile=cf, instrument=ticker,
+                           queryParameters=arguments))
+    pprint(Candles.to_df(configFile=cf, instrument=ticker,
+                         queryParameters=arguments))
