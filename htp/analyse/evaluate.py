bf9331f8bbace71046c4a572e7fc86d4732245db
Jules Kirk <kirkjules.jk@gmail.com>
2019-06-16 09:01:06 +1000

    complete buy_signal_cross with dependent functions and script function to display result. Needs testing.

--- htp/analyse/evaluate.py
+++ htp/analyse/evaluate.py
@@ -5,32 +5,50 @@ import numpy as np
 def entry_exit_combine(row, df, entry, exit):
 
     if row[entry] == True:
-        reduct = df.loc[row.name:]
+        reduct = df[row.name:]
         exit = reduct[reduct[exit] == True].iterrows()
         try:
-            trade_dt = [row.name, next(exit)[0]]
+            return [row.name, next(exit)[0]]
         except StopIteration:
-            trade_dt = [row.name, np.nan]
-
-    return trade_dt
+            return [row.name, np.nan]
+    else:
+        return [np.nan, np.nan]
 
 
 def buy_signal_cross(df, fast, slow):
 
     system = "{} > {}".format(fast, slow)
-    signal = df.apply(lambda x: x[fast] > x[slow], axis=1).rename(system)
-    signal["prev"] = df[system].shift(2)
-    signal["curr"] = df[system].shift(1)
+    signal = df.apply(
+        lambda x: x[fast] > x[slow], axis=1).rename(system).to_frame()
+    signal["prev"] = signal[system].shift(2)
+    signal["curr"] = signal[system].shift(1)
     signal["entry"] = signal.apply(
         lambda x: x["prev"] is False and x["curr"] is True, axis=1)
     signal["exit"] = signal.apply(
         lambda x: x["prev"] is True and x["curr"] is False, axis=1)
     trade = signal.apply(entry_exit_combine, axis=1, result_type="expand",
-                         args=(df, "entry", "exit"))
-
-    return trade
+                         args=(signal, "entry", "exit")).rename(
+                             columns={0: "entry", 1: "exit"})
+    entry_exit = trade[~trade["entry"].isnull()].reset_index(drop=True)
+    return entry_exit
 
 
 def sell_signal_cross(df, fast, slow):
-
     pass
+
+
+if __name__ == "__main__":
+
+    from htp.api import oanda
+    from htp.analyse import indicator
+    ticker = "AUD_JPY"
+    arguments = {"from": "2018-02-05T22:00:00.000000000Z",
+                 "granularity": "D",
+                 "smooth": True,
+                 "count": 50}
+    data = oanda.Candles.to_df(instrument=ticker, queryParameters=arguments)
+    sma_5 = indicator.smooth_moving_average(data, column="close", period=5)
+    sma_5_10 = indicator.smooth_moving_average(data, df2=sma_5, column="close",
+                                               concat=True, period=10)
+    entry_exit = buy_signal_cross(sma_5_10, "close_sma_5", "close_sma_10")
+    print(entry_exit)
