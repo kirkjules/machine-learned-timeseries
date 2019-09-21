e3795285650ca53aa3c066371af883d3f405ea21
Jules Kirk <kirkjules.jk@gmail.com>
2019-09-21 11:44:01 +1000

    removed legacy engine.py module and renamed workshop to engine.py

--- htp/toolbox/engine.py
+++ htp/toolbox/engine.py
@@ -1,257 +1,319 @@
-import os
-import copy
-import logging
+# import sys
 import multiprocessing
 from queue import Queue
+from functools import partial
 from threading import Thread, Lock
-from htp.api import exceptions
-
-log = logging.getLogger(__name__)
 
 
-class Worker(Thread):
+class Worker:
+    """
+    Class that defines a wrapper that actions a target function over elements
+    presented in an iterable data structure in a sequential process.
 
-    def __init__(self, date_gen, func, **kwargs):
+    This is also the base class in the module from which initial attributes are
+    inherited and used to establish wrappers that action concurrent and
+    parallel processes respectively.
+
+    See Also
+    --------
+    ConcurrentWorker
+    ParallelWorker
+    """
+    def __init__(self, func, iterable_arg, *args, iterable=[], **kwargs):
         """
-        :param func: the function that wraps the api  whose arguments are
-        parsed as kwargs.
-        :param kwargs: where func arguments are parsed. One of these arguments
-        will be the queryParameters dictionary that is modified by data present
-        in the queue.
+        Class initialiser to establish the target function, the iterable, and
+        any positional or keywork arguments that should be parsed to the
+        function.
+
+        Parameters
+        ----------
+        func
+            This can be any function that accepts a keyword argument that could
+            be stored in an iterable data structure.
+
+        iterable_arg : str
+            The keyword that references the iterable argument in the target
+            function.
+
+        iterable : list
+            An iterable containing elements that are individually parsed to the
+            target function as a keyword argument.
+
+        *args, **kwargs
+            Any positional or keyword arguments required by the target function
+            and remain constant.
+
+        Attributes
+        ----------
+        func
+            The function with, if any, positional and keyword arguments
+            included.
+
+        iterable
+            The iterable parsed above.
+
+        iterable_arg
+            The target function's keyword tha parses the iterable's element.
+
+        Examples
+        --------
+        >>> from copy import deepcopy
+        >>> from htp.api.oanda import Candles
+        >>> from htp.toolbox.dates import Select
+        >>> instrument = "AUD_JPY"
+        >>> func = Candles.to_df
+        >>> queryParameters = {"granularity": "D"}
+        >>> date_gen = Select().by_month(
+        ...     period=2, no_days=[5, 6], year_by_day=True)
+        >>> date_list = []
+        >>> for i in date_gen:
+        ...     queryParameters["from"] = i["from"]
+        ...     queryParameters["to"] = i["to"]
+        ...     date_list.append(deepcopy(queryParameters))
+        >>> d = Worker(func, "queryParameters", iterable=date_list,
+        ...     instrument=instrument)
+        >>> print(d.func)
+        functools.partial(<bound method Candles.to_df of <class \
+'htp.api.oanda.Candles'>>, instrument='AUD_JPY')
         """
-        self.date_gen = date_gen
-        self.func = func
-        self.kwargs = kwargs
-        self.arg_list = []
-        for parameter_set in self.date_gen:
-            sub_list = {}  # []
-            # sub_list.append(self.func)
-            sub_list["func"] = self.func
-            for parameter in parameter_set.keys():
-                self.kwargs["queryParameters"][parameter] = \
-                        parameter_set[parameter]
-            # sub_list.append(self.kwargs)  # kwargs)
-            for argument in self.kwargs.keys():
-                sub_list[argument] = self.kwargs[argument]
-            self.arg_list.append(copy.deepcopy(sub_list))
+        self.func = partial(func, *args, **kwargs)
+        self.iterable = iterable
+        self.iterable_arg = iterable_arg
 
-    def run(self):
+    def seq(self):
         """
-        Function to sequentially download ticker data as defined by each
-        dataset in the iterable self.arg_list.
-        The function intentially uses a for loop. For more optimized methods,
-        threading and multiprocessing options are defined below.
+        To execute the target function sequentially across the given iterable's
+        elements, with the provided positional and keyword arguments.
+
+        Returns
+        -------
+        list
+            A list, where the elements represent the respective results from
+            calling the target function on each value stored in the iterable.
+
+        Examples
+        --------
+        >>> from copy import deepcopy
+        >>> from htp.api.oanda import Candles
+        >>> from htp.toolbox.dates import Select
+        >>> instrument = "AUD_JPY"
+        >>> func = Candles.to_df
+        >>> queryParameters = {"granularity": "D"}
+        >>> date_gen = Select().by_month(
+        ...     period=2, no_days=[5, 6], year_by_day=True)
+        >>> date_list = []
+        >>> for i in date_gen:
+        ...     queryParameters["from"] = i["from"]
+        ...     queryParameters["to"] = i["to"]
+        ...     date_list.append(deepcopy(queryParameters))
+        >>> d = Worker(func, "queryParameters", iterable=date_list,
+        ...     instrument=instrument).seq()
+        >>> print(d[1].head())
+                               open    high     low   close
+        2019-06-02 21:00:00  75.068  75.487  74.968  75.401
+        2019-06-03 21:00:00  75.396  75.696  75.082  75.606
+        2019-06-04 21:00:00  75.604  75.904  75.404  75.585
+        2019-06-05 21:00:00  75.594  75.776  75.280  75.628
+        2019-06-06 21:00:00  75.632  75.817  75.492  75.738
         """
-        results = []
-        for iterable in self.arg_list:
-            func = iterable.pop("func")
-            resp = None
-            try:
-                data = func(**iterable)
-            except exceptions.OandaError as e:
-                resp = e.oanda_msg
-            except exceptions.ApiError as e:
-                resp = e
-                log.info(resp)
-            else:
-                resp = data  # .df()
-            finally:
-                log.info("{0}, {1}: {2}".format(
-                    os.getpid(), iterable["queryParameters"]["from"]
-                    .replace(".000000000Z", ""), iterable["queryParameters"]
-                    ["to"].replace(".000000000Z", "")))
-                results.append(resp)  # k
+        st = []
+        for i in iter(self.iterable):
+            st.append(self.func(**{self.iterable_arg: i}))
 
-        return results
+        return st
 
 
-class ConcurrentWorker(Worker):  # Thread
+class ConcurrentWorker(Worker):
+    """
+    Class that inherit from `Worker` and subsequently provides concurrent
+    processing functionality to a target function.
 
-    def __init__(self, date_gen, func, **kwargs):
-        super().__init__(date_gen, func, **kwargs)
-        self.__lock = Lock()
-        self.__q = Queue()
-
-    def __thread_func(self, iterable):  # parameter_set):
+    See Also
+    --------
+    Worker
+    """
+    def __init__(self, func, iterable_arg, *args, **kwargs):
         """
-        Private function that wraps api function.
-        :param iterable: the item/job as it is parsed from the queue of
-        jobs. Specifically it is a list type item whose values correspond to
-        arguments in the target api function.
-        """
-        func = iterable.pop("func")
-        resp = None
-        try:
-            data = func(**iterable)
-        except exceptions.OandaError as e:
-            resp = e.oanda_msg
-        except exceptions.ApiError as e:
-            resp = e
-            with self.__lock:
-                log.info(resp)
-        else:
-            resp = data  # .df()
-        finally:
-            with self.__lock:
-                log.info("{0}, {1}: {2}".format(
-                    os.getpid(), iterable["queryParameters"]["from"]
-                    .replace(".000000000Z", ""), iterable["queryParameters"]
-                    ["to"].replace(".000000000Z", "")))
-        return resp
+        Class initialiser that inherits from the `Worker` class and assigns
+        private attributes required to concurrently process a target function
+        across given elements in an iterable.
 
-    def __threader(self, d):
+        Attributes
+        ----------
+        _lock
+        _queue
+
+        See Also
+        --------
+        Worker.__init__
+        """
+        super().__init__(func, iterable_arg, *args, **kwargs)
+        self._lock = Lock()
+        self._queue = Queue()
+
+    def _threader(self, st):
+        """
+        Threader function that feeds items from the queue to the target
+        function.
+
+        Parameters
+        -----------
+        st : list
+            An empty list into which the returned values from the target
+            function are stored.
+        """
         while True:
-            # get the job from the front of the queue
-            item = self.__q.get()
+            item = self._queue.get()
             if item is None:
                 break
-            d.append(self.__thread_func(item))
-            self.__q.task_done()
+            st.append(self.func(**{self.iterable_arg: item}))
+            self._queue.task_done()
 
-    def run(self):
+    def crt(self):
         """
-        Method to run the target function as wrapped by __thread_func and
-        directed by __threader, concurrently, on multiple threads.
+        Function that assembles the required components to action the target
+        function concurrently against an iterable's elements stored in a queue.
+
+        Returns
+        -------
+        list
+            A list of elements, each the respective result of the target
+            function working on a given value present in the iterable.
         """
         t = []
         results = []
         for i in range(4):
-            thread = Thread(target=self.__threader, args=(results,))
-            # this ensures the thread will die when the main thread dies
-            # can set t.daemon to False to keep running
+            thread = Thread(target=self._threader, args=(results,))
             thread.daemon = True
             thread.start()
             t.append(thread)
 
-        self.__q.join()
+        self._queue.join()
 
-        for job in self.arg_list:
-            self.__q.put(job)
+        for job in self.iterable:
+            self._queue.put(job)
 
         for i in range(4):
-            self.__q.put(None)
+            self._queue.put(None)
 
         for threads in t:
             threads.join()
 
-        # pprint(results)
         return results
 
 
 class ParallelWorker(Worker):
+    """
+    Class that inherit from `Worker` and subsequently provides parallel
+    processing functionality to a target function.
 
-    def __init__(self, date_gen, func, **kwargs):
+    See Also
+    --------
+    Worker
+    """
+    def __init__(self, func, iterable_arg, *args, **kwargs):
         """
         ParallelWorker is a class that inherits from Worker for necessary
         attributes and then provides a pool of workers for the parsed
         task and arguments to be worked on in parallel.
-        """
-        super().__init__(date_gen, func, **kwargs)
 
-    def init_lock(self, l_):
+        See Also
+        --------
+        Worker.__init__
+        """
+        super().__init__(func, iterable_arg, *args, **kwargs)
+
+    def _init_lock(self, l_):
         """
         Lock initialiser used in the pool setup.
         """
         global lock
         lock = l_
 
-    def target(self, iterable):
+    def _arg_kw(self, func, k, iterable):
         """
-        Target function that wraps the original api function.
-        :param iterable: a list that contains to values, the first is the
-        function name that will interact with the api, the second is a
-        dictionary with keys matching the api function arguments.
+        Internal helper function to parse the elements stored in an iterable as
+        keyword arguments in the target function.
         """
-        func = iterable.pop("func")
-        resp = None
-        try:
-            data = func(**iterable)
-        except exceptions.OandaError as e:
-            resp = e.oanda_msg
-        except exceptions.ApiError as e:
-            lock.acquire()
-            resp = e
-            log.info(resp)
-            lock.release()
-        else:
-            resp = data  # .df()
-        finally:
-            lock.acquire()
-            log.info("{0}, {1}: {2}".format(os.getpid(),
-                                            iterable["queryParameters"]["from"]
-                                            .replace(".000000000Z", ""),
-                                            iterable["queryParameters"]["to"]
-                                            .replace(".000000000Z", "")))
-            lock.release()
-        return resp  # k
+        return func(**{k: iterable})
 
-    def run(self):  # (d, date_gen, func, **kwargs)
+    def prl(self, lock_func=None, lock_arg=None):
         """
         Method to run target function in parallel. The pool of workers is
         initialised with a lock that is used for logging in the target
         function.
+
+        Returns
+        -------
+        list
+            A list of elements, each the respective result of the target
+            function working on a given value present in the iterable.
         """
-        l_ = multiprocessing.Lock()
-        pool = multiprocessing.Pool(processes=4,
-                                    initializer=self.init_lock,
-                                    initargs=(l_,))
+        if lock_func is None:
+            lock_func = self._init_lock
+            lock_arg = multiprocessing.Lock()
+
+        pool = multiprocessing.Pool(
+            processes=3, initializer=lock_func, initargs=(lock_arg,))
         results = []
-        for i in pool.map(self.target, self.arg_list):
+        for i in pool.map(partial(self._arg_kw, self.func, self.iterable_arg),
+                          self.iterable):
             results.append(i)
 
         pool.close()
         pool.join()
 
-        # pprint(results)
         return results
 
 
 if __name__ == "__main__":
+    import os
     import time
-    # from pprint import pprint
-    from htp.api import oanda
-    from htp.toolbox import dates
+    import pandas as pd
+    from loguru import logger
+    from copy import deepcopy
+    from pprint import pprint
+    from htp.api.oanda import Candles
+    from htp.toolbox.dates import Select
 
-    f = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
-    logging.basicConfig(level=logging.INFO, format=f)
+    # f = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+    # logging.basicConfig(level=logging.INFO, format=f)
 
-    func = oanda.Candles.to_df
-    date_gen = dates.Select().by_month(period=5, no_days=[5, 6],
-                                       year_by_day=True)
+    logger.enable("htp.api.oanda")
+
+    cf = os.path.join(os.path.dirname(__file__), "../..", "config.yaml")
+    instrument = "AUD_JPY"
+    func = Candles.to_df
+    queryParameters = {"granularity": "D"}
+    # date_gen = Select().by_month(period=5, no_days=[6], year_by_day=True)
+    date_gen = Select(
+        from_="2019-03-04 21:00:00", to="2019-06-15 22:00:00",
+        local_tz="America/New_York").by_month()
     date_list = []
     for i in date_gen:
-        date_list.append(i)
-
-    instrument = "AUD_JPY"
-    queryParameters = {"granularity": "D"}
-    cf = os.path.join(os.path.dirname(__file__), "../..", "config.yaml")
-
+        queryParameters["from"] = i["from"]
+        queryParameters["to"] = i["to"]
+        date_list.append(deepcopy(queryParameters))
+    # sys.exit()
     start_time = time.time()
-    d = Worker(date_gen=date_list,
-               func=func,
-               configFile=cf,
-               instrument=instrument,
-               queryParameters=queryParameters).run()
-    # pprint(d)
+    d = ParallelWorker(
+        func, "queryParameters", iterable=date_list, configFile=cf,
+        instrument=instrument).prl()
+    pprint(pd.concat(d, axis=0))
     print("--- %s seconds ---" % (time.time() - start_time))
+
     """
     start_time = time.time()
-    print("ConcurrentWorker\n")
-    d = ConcurrentWorker(d=d,
-                         date_gen=date_list,
-                         func=func,
-                         configFile=cf,
-                         instrument=instrument,
-                         queryParameters=queryParameters).run()
+    d = ConcurrentWorker(
+        func, "queryParameters", iterable=date_list, configFile=cf,
+        instrument=instrument).crt()
+    pprint(d)
     print("--- %s seconds ---" % (time.time() - start_time))
 
-    print("\nParallelWorker\n")
     start_time = time.time()
-    ParallelWorker(date_gen=date_list,
-                   func=func,
-                   configFile=cf,
-                   instrument=instrument,
-                   queryParameters=queryParameters).run()
+    d = Worker(
+        func, "queryParameters", iterable=date_list, configFile=cf,
+        instrument=instrument).seq()
     pprint(d)
     print("--- %s seconds ---" % (time.time() - start_time))
     """
