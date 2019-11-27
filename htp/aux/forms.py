6826e803276f849f181ec73512856319ed78babc
Jules Kirk <kirkjules.jk@gmail.com>
2019-11-27 20:27:16 +1100

    css and js styling and functionality applied to get data and indicator forms.

--- htp/aux/forms.py
+++ htp/aux/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField, \
-    SelectMultipleField, DateTimeField
+    SelectMultipleField, SelectField, DateTimeField
 from wtforms.validators import DataRequired
 
 
@@ -8,26 +8,28 @@ class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Remember Me')
-    submit = SubmitField('Sign In')
+    submit = SubmitField('Submit')
 
 
 class CandlesForm(FlaskForm):
     ticker = StringField('Ticker', validators=[DataRequired()])
     price = SelectMultipleField(
         'Price', validators=[DataRequired()], choices=[
-            ("M", "Mid"), ("A", "Ask"), ("B", "Bid")])
+            ("M", "Mid"), ("A", "Ask"), ("B", "Bid")],
+        render_kw={"class_": "chosen-select"})
     from_ = DateTimeField('From', validators=[DataRequired()])
     to = DateTimeField('To', validators=[DataRequired()])
     granularity = SelectMultipleField(
         'Granularity', validators=[DataRequired()], choices=[
-            ("M15", "M15"), ("H1", "H1"), ("H4", "H4")])
+            ("M15", "M15"), ("H1", "H1"), ("H4", "H4")],
+        render_kw={"class_": "chosen-select"})
     smooth = BooleanField('Smooth')
-    submit = SubmitField('Acquire')
+    submit = SubmitField('Submit')
 
 
 class IndicateForm(FlaskForm):
     ticker = StringField('Ticker', validators=[DataRequired()])
-    granularity = SelectMultipleField(
+    granularity = SelectField(
         'Granularity', validators=[DataRequired()], choices=[
             ("M15", "M15"), ("H1", "H1"), ("H4", "H4")])
-    submit = SubmitField('Acquire')
+    submit = SubmitField('Submit')
